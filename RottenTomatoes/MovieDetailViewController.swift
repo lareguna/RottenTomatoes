8c07b5475947a4b7d9b1f64c18640ccbbf28b24c
Lakshmi <lakshmi.reguna@yahoo.com>
2014-10-11 22:42:50 -0700

    Added refresh control

--- RottenTomatoes/MovieDetailViewController.swift
+++ RottenTomatoes/MovieDetailViewController.swift
@@ -26,12 +26,39 @@ class MovieDetailViewController: UIViewController {
     
     @IBOutlet weak var movieCast: UILabel!
     
-    var index : Int? = nil  // the index into movies; always set by something else
+    var movieID: String? = ""
+    var index : Int? = 0  // the index into movies; always set by something else
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
+        var apiKey: String = "et5rj7cxytpx9e5faamzxmmv"
+        
+        var url: String = "http://api.rottentomatoes.com/api/public/v1.0/movies/" + movieID!+".json?apikey=" + apiKey
+        var request = NSURLRequest(URL: NSURL(string: url))
+        
+        println("In detailedMovie Controller movieID \(movieID)")
+        println("In detailedMovie Controller index \(index)")
+        
+        NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue.mainQueue()) { (response: NSURLResponse!, data: NSData!, error: NSError!) -> Void in
+            var object = NSJSONSerialization.JSONObjectWithData(data, options: nil, error: nil) as NSDictionary
+            
+            println("object: \(object)")
+            self.title = object["title"] as? String
+            self.movieName.text = self.title
+            println("movie title \(self.title)")
+            self.movieSynopsis.text = object["synopsis"] as? String
+            
+            // setup ratings
+            var ratings = object["ratings"] as NSDictionary
+            println("ratings: \(ratings)")
+
+            //self.criticsRating.text = ratings["critcs_rating"] as? Int
+            self.audienceScore.text = ratings["audience_score"] as? String
+        
+            
+            //self.tableView.reloadData()
+        }
     }
 
     override func didReceiveMemoryWarning() {
--- RottenTomatoes/MoviesViewController.swift
+++ RottenTomatoes/MoviesViewController.swift
@@ -13,15 +13,48 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
     
     @IBOutlet weak var tableView: UITableView!
     var movies: [NSDictionary] = []
+    var movieID: String = ""
+    var refreshControl:UIRefreshControl!
+
+    
+    func getData() {
+        NSLog("getData called")
+        var apiKey = "et5rj7cxytpx9e5faamzxmmv"
+        var url = "http://api.rottentomatoes.com/api/public/v1.0/lists/movies/box_office.json?apikey="+apiKey+"&limit=20&country=us"
+        var request = NSURLRequest(URL: NSURL(string: url))
+        
+        NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue.mainQueue() ) {
+            (response: NSURLResponse!, urlData: NSData!, error: NSError!) -> Void in
+            
+            if urlData != nil {
+                var err: NSError?
+                if let object = (NSJSONSerialization.JSONObjectWithData(urlData, options: nil, error: &err) as? NSDictionary) {
+                    NSLog("object valid")
+                    // data was returned, populate dictionary
+                    self.movies = object["movies"] as [NSDictionary]
+                } else {
+                    NSLog("object NOT valid")
+                    // there was an error
+                    //self.showAlert()
+                }
+                self.tableView.reloadData()
+                self.refreshControl.endRefreshing()
+            } else {
+                // network error
+                
+            }
+            //println("object = \(object)")
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // This causes you to register for all the tableView events.
         tableView.delegate = self
-        
         tableView.dataSource = self
-
+/*
         var url = "http://api.rottentomatoes.com/api/public/v1.0/lists/movies/box_office.json?apikey=et5rj7cxytpx9e5faamzxmmv&limit=20&country=us"
+ 
         
         var request = NSURLRequest(URL: NSURL(string: url))
         
@@ -29,11 +62,24 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
             var object = NSJSONSerialization.JSONObjectWithData(data, options: nil, error: nil) as NSDictionary
     
             self.movies = object["movies"] as [NSDictionary]
-            println("\(self.movies.count)")
-            println("object: \(object)")
-            self.tableView.reloadData()
+            //println("\(self.movies.count)")
+            //println("object: \(object)")
+            //self.tableView.reloadData()
+*/
+            // setup refresh control
+            self.refreshControl = UIRefreshControl()
+            self.refreshControl.backgroundColor = UIColor.orangeColor()
+            self.refreshControl.tintColor = UIColor.whiteColor()
+            self.refreshControl.attributedTitle = NSAttributedString(string: "Pull to Refresh")
+            self.refreshControl.addTarget(self, action: "getData", forControlEvents: UIControlEvents.ValueChanged)
+            self.tableView.addSubview(self.refreshControl)
             
-        }
+            // get data from network, has to come after refreshControl is setup
+            self.getData()
+        //}
+
+        
+
     }
     
     override func didReceiveMemoryWarning() {
@@ -46,11 +92,12 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        println("I'm at row: \(indexPath.row), section  \(indexPath.section)")
+        //println("I'm at row: \(indexPath.row), section  \(indexPath.section)")
         
         var cell = tableView.dequeueReusableCellWithIdentifier("MovieCell") as MovieCell
-      
         var movie = movies[indexPath.row]
+        
+        self.movieID = movie["id"] as String
         
         cell.movieTitleLabel.text = movie["title"] as?  String
         cell.synopsisLabel.text = movie["synopsis"] as? String
@@ -61,10 +108,7 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         
         cell.posterView.setImageWithURL(NSURL(string: posterUrl))
         
-
-        
-        
-  //      cell.textLabel!.text = "Hello I'm at row: \(indexPath.row), section  \(indexPath.section)"
+  //    cell.textLabel!.text = "Hello I'm at row: \(indexPath.row), section  \(indexPath.section)"
         
         return cell
     }
@@ -78,12 +122,16 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         // Pass the selected object to the new view controller.
     
     */
+    
+
       override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject!) {
-            if segue.identifier == "Detail Segue" {
-                var row = self.tableView.indexPathForSelectedRow()?.row
+            if segue.identifier == "MovieDetailSegue" {
+                var selectedRow:Int = self.tableView.indexPathForSelectedRow()?.row as Int!
+                //let viewController = segue.destinationViewController as MovieDetailViewController
                 var detail = segue.destinationViewController as MovieDetailViewController
-                detail.index = row
-                println("Seque to \(detail.title), setting index to \(row)")
+                detail.index = selectedRow
+                detail.movieID = self.movieID
+                println("Seque to \(detail.index), setting index to \(selectedRow)")
             }
         }
 
